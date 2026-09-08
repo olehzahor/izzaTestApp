ac0e19bf0adea2da4f6537461e629cb6a6af4d03
Oleh Zahoriansky <olehzahor@gmail.com>
2026-09-08 15:21:12 +0300

    Initial Commit

---
+++ izza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  izza
+//
+//  Created by Oleh on 08.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ izza/izzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  izzaApp.swift
+//  izza
+//
+//  Created by Oleh on 08.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct izzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
